b1a14dacafb8b231c521ea646b47fbe159c3f7ba
Daniil Bolotov <somerapmails@yandex.ru>
2020-12-07 01:58:04 +0300

    separated and removed redundant dependencies

--- src/test/java/com/exyfi/onlineshop/servlet/AddProductServletTest.java
+++ src/test/java/com/exyfi/onlineshop/servlet/AddProductServletTest.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
